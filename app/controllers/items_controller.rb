ab9eb276ce03029a3938f184370f12044b39ae92
syun-momo <72676307+syun-momo@users.noreply.github.com>
2021-01-12 21:35:12 +0900

    ユーザー管理機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,17 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
 end
