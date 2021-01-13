869b128e6dd7a9354173e5474ddc7134e2dac991
syun-momo <72676307+syun-momo@users.noreply.github.com>
2021-01-13 21:36:45 +0900

    モデル単体テストコード準備

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
+  validates :nickname, presence: true
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: '全角文字を使用してください' } do
+    validates :family_name_kanji
+    validates :first_name_kanji
+  end
+  with_options presence: true, format: { with: /\A[ァ-ヶ]+\z/, message: '全角カナ文字を使用してください' } do
+    validates :family_name_kana
+    validates :first_name_kana
+  end
+  validates :birthday, presence: true
 end
