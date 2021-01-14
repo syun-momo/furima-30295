1973a8c24f38fe78b5fad8e1d9f2e6a4aec504b5
syun-momo <72676307+syun-momo@users.noreply.github.com>
2021-01-14 23:01:06 +0900

    READMEと照らし合わせ

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,12 +7,12 @@ class User < ApplicationRecord
   validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
   validates :nickname, presence: true
   with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: '全角文字を使用してください' } do
-    validates :family_name_kanji
-    validates :first_name_kanji
+    validates :family_name
+    validates :first_name
   end
   with_options presence: true, format: { with: /\A[ァ-ヶ]+\z/, message: '全角カナ文字を使用してください' } do
     validates :family_name_kana
     validates :first_name_kana
   end
-  validates :birthday, presence: true
+  validates :birth_date, presence: true
 end
