36770434a9c7face2b5420cd30c9a8edb98b6082
syun-momo <72676307+syun-momo@users.noreply.github.com>
2021-02-11 21:38:55 +0900

    商品編集レビュー修正１

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_item, only: [:edit, :update]
 
   def index
     @items = Item.all.order('created_at DESC')
@@ -23,8 +24,6 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    @item = Item.find(params[:id])
-    redirect_to root_path if current_user.id != @item.user_id
   end
 
   def update
@@ -40,5 +39,10 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:image, :name, :info, :category_id, :status_id, :shopping_fee_status_id, :prefecture_id,
                                  :scheduled_delivery_id, :price).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
+    redirect_to root_path if current_user.id != @item.user_id
   end
 end
