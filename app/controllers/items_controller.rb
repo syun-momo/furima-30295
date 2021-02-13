52650e806d33e37a05ae87416b83dd45ccdcb6e2
syun-momo <72676307+syun-momo@users.noreply.github.com>
2021-02-13 18:04:46 +0900

    削除機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -36,6 +36,12 @@ class ItemsController < ApplicationController
     end
   end
 
+  def destroy
+    item = Item.find(params[:id])
+    item.destroy
+    redirect_to root_path
+  end
+
   def item_params
     params.require(:item).permit(:image, :name, :info, :category_id, :status_id, :shopping_fee_status_id, :prefecture_id,
                                  :scheduled_delivery_id, :price).merge(user_id: current_user.id)
