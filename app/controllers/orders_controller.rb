9ab075f85f8e98c9198bfbbbaa708f4bbe46368d
syun-momo <72676307+syun-momo@users.noreply.github.com>
2021-02-15 22:33:48 +0900

    購入画面情報表示

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,9 +1,17 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_item, only: [:index]
+
   def index
     @user_order = UserOrder.new
   end
 
   def create
   end
-  
+
+  private
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
 end
