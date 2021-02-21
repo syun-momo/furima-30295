f3498dcf9cbaf3d63979324f5ddfdf673e982b43
syun-momo <72676307+syun-momo@users.noreply.github.com>
2021-02-21 16:02:13 +0900

    テストコード初期挙動確認

--- spec/factories/user_orders.rb
+++ spec/factories/user_orders.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
   factory :user_order do
-    
+    postal_code { '111-1111' }
+    prefecture_id { 2 }
+    city { '札幌市' }
+    address { '西区' }
+    building { 'エスポワール' }
+    phone_number { 11111112222 }
+    token { 'tokentokentoken' }
+    user_id { 1 }
+    item_id { 2 }
   end
 end
--- spec/models/user_order_spec.rb
+++ spec/models/user_order_spec.rb
@@ -1,5 +1,53 @@
 require 'rails_helper'
 
 RSpec.describe UserOrder, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @user_order = FactoryBot.build(:user_order)
+  end
+
+  describe '商品購入' do
+
+    it '全ての値が揃っている時住所登録できる' do
+      expect(@user_order).to be_valid
+    end
+
+    it '建物が空でも登録できる' do
+      @user_order.building = ''
+      expect(@user_order).to be_valid
+    end
+  end
+
+  context '商品購入がうまく行かない時' do
+    it '郵便番号が空では決済できないこと' do
+    end
+
+    it '都道府県が空では決済できないこと' do
+    end
+
+    it '市区町村が空では決済できないこと' do
+    end
+
+    it '番地が空では決済できないこと' do
+    end
+
+    it '電話番号が空では決済できないこと' do
+    end
+
+    it '電話番号にはハイフンは不要であること' do
+    end
+
+    it '電話番号は11桁以内であること' do
+    end
+
+    it 'tokenが空では登録できないこと' do
+    end
+
+    it 'user_idが空だと登録できない' do
+    end
+
+    it 'item_idが空だと登録できない' do
+    end
+  end
+
+  
 end
