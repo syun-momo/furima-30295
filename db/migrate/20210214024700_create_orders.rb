6cd0357bc110baa4c0819d01cd273fdd3d4b8e63
syun-momo <72676307+syun-momo@users.noreply.github.com>
2021-02-22 11:24:01 +0900

    stash解消正常に

--- db/migrate/20210214024700_create_orders.rb
+++
@@ -1,8 +0,0 @@
-class CreateOrders < ActiveRecord::Migration[6.0]
-  def change
-    create_table :orders do |t|
-      t.references :user_item,  foreign_key: true
-      t.timestamps
-    end
-  end
-end
